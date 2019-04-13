e195534fc8c7afd8c360e585382ee75a5bfe4e17
utkarsh4squarelogic <utkarshraj@4squarelogic.info>
2019-04-13 12:22:48 +0530

    Some Config File has been Changes

--- src/Config.ts
+++ src/Config.ts
@@ -1,14 +1,14 @@
 export class Config {
     public static site = {
         port: 9090,
-        baseUrl: `http://localhost:9090/`
+        baseUrl: `http://localhost:9000/`
     };
     public static db: any = {
         host: `highmountains.ctifvjah2djq.ap-south-1.rds.amazonaws.com`,
         port:"3306",
-        user: "high_mountains",
+        user: "TestingDb",
         database: "high_mountains",
-        password:"high_mountains"
+        password:"TestingDb"
     
     };
     public static gcmAPIKey: string = "AAAAFlPX7b0:APA91bH6eWFzowQueJiUPPSsiMxr_1FAzQffkTKlOX98Uh3VVuyz4SaH5kJ9iLGNRep_bEw3kq-YSY7X2yTxidpeKkRiHNSIhYVfayCAyytTkV6Y2qgtguwUNjrhEbBwlg2boTkzDo_U";
