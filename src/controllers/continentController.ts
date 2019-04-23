f277b7f3f4b3dc649dd8fa808c37453d4a484bf2
utkarsh4squarelogic <utkarshraj@4squarelogic.info>
2019-04-23 16:32:44 +0530

    All controller and ts has been added

---
+++ src/controllers/continentController.ts
@@ -0,0 +1,22 @@
+import {Request, Response} from "express";
+import {BaseController} from "./common/BaseController";
+import SqlService from "../services/common/SQLService";
+import {Tables} from "../database/Tables";
+import * as _ from "lodash";
+import * as __ from "underscore";
+
+export class continentController extends BaseController {
+    private sqlService: SqlService;
+
+    constructor() {
+        super();
+        this.sqlService = new SqlService();
+    }
+
+    public getAllSpringFieldData(req: Request, res: Response) {
+        const user = this.sqlService.executeQuery(`select * from ${Tables.springfield};`);
+        this.sendResponse(user, res);     
+       }
+
+
+}
