d2ed057e6973a540d7671a4c8c93ba3c5ed127df
inderjots1 <inderjot.android@4squarelogic.info>
2019-06-13 12:51:19 +0530

    Review Add Model Update

--- src/models/RevivewModel.ts
+++ src/models/RevivewModel.ts
@@ -3,6 +3,10 @@ export interface RevivewModel{
     imageUrl?:String;
     review?:String; 
     rating?:number; 
+    type?:number;
+    u_name?:String;
+    time?:String;
+    u_image ?:String
     dest_id?:number; 
     user_id?:number; 
     others?:string;   
